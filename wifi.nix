6f10f9d4aa8747517fda22c6dbf364d2b2004896
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-03 15:49:09 -0500

    updated configs

--- wifi.nix
+++ wifi.nix
@@ -9,7 +9,7 @@ networking.nameservers = [
 ];
 
 networking.networkmanager.ensureProfiles = {
-   environmentFiles = ["~/ranger097_nixos_dotfiles/wifi.env"];
+   environmentFiles = ["/home/ranger/ranger097_nixos_dotfiles/wifi.env"];
    profiles = {
       Home = {
          connection = { id = "$WIFI_ID_HOME"; type = "wifi"; };
