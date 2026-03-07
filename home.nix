20af13ee70a26b0a391029c9c9264600f59ba0bd
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-07 11:57:49 -0500

    updated configs

--- home.nix
+++ home.nix
@@ -34,7 +34,7 @@ wayland.windowManager.hyprland = {
   
   settings = {
     monitor = if osConfig.networking.hostName == "jirachi" 
-      then [ "eDP-1, 3840x2400@59.9, 0x0, 2.5" ]
+      then [ "eDP-1, 3840x2400@59.9, 0x0, 3" ]
       else if osConfig.networking.hostName == "deoxy"
       then [ "eDP-1, 1920x1080@60.054, 0x0, 0.5" ]
       else [ ",preferred,auto,1" ];
