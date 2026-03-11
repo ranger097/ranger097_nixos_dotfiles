66e6ebda0a7c3fca0636f8afe9837ae6cb1eb3a3
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-11 17:01:04 -0400

    updated configs

--- bash.nix
+++ bash.nix
@@ -77,8 +77,8 @@ echo " Upgrading Nixos System"
 nix flake update
 }
 
-
-export LS_COLORS="${LS_COLORS}:su=30;41:ow=30;42:st=30;44:"
+(cat ~/.cache/wal/sequences &)
+source ~/.cache/wal/colors-tty.sh
 wal -i /home/ranger/ranger097_nixos_dotfiles/wallpapers/Guweiz/wallhaven-d85gjg.jpg &> /dev/null 
 #fastfetch   
 pokeget random --hide-name
