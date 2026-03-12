265db3b419816ed41fe35cc2c8897db03405da18
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-12 02:34:06 -0400

    updated configs

--- home.nix
+++ home.nix
@@ -94,8 +94,8 @@ gaps_out = 0,10,0,10
 border_size = 1
 
 #BORDER
-col.active_border = $color3
-col.inactive_border = $color3
+col.active_border = $color0
+col.inactive_border = $color0
 #col.active_border = rgba(0,0,0,1)
 #col.inactive_border = rgba(0,0,0,1)
 
